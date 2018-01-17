928ba0a9464fb539e9ded16acdabcb76aee0b452
kaytyron <kelvin.tyron@gmail.com>
2018-01-17 09:24:53 +0000

    Begining of Study Commit

---
+++ app/src/main/java/com/example/knuma/recyclerviewandconnection/Movie.java
@@ -0,0 +1,42 @@
+package com.example.knuma.recyclerviewandconnection;
+
+/**
+ * Created by knuma on 17/1/18.
+ */
+
+public class Movie {
+    private String title, genre, year;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String genre, String year) {
+        this.title = title;
+        this.genre = genre;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String name) {
+        this.title = name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
